d9090a9cb2de116ad43a52e657239391130bbbb9
Aaron Helton <mariusagricola@gmail.com>
2014-10-09 17:33:59 +0000

    init

--- src/main/java/Transform.java
+++ src/main/java/Transform.java
@@ -1,3 +1,24 @@
+import java.io.*;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+@WebServlet("/Transform")
 public class Transform
 {
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    String url = request.getParameter("url");
+    String format = request.getParameter("format");
+  }
 }
