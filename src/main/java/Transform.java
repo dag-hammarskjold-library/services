a8efb6c9899f99552818f1c51a04363e03f8dad8
Aaron Helton <mariusagricola@gmail.com>
2014-10-10 09:25:32 -0400

    Update Transform.java

--- src/main/java/Transform.java
+++ src/main/java/Transform.java
@@ -18,7 +18,24 @@ import org.xml.sax.SAXException;
 public class Transform
 {
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    // First let's extract these if they're present in the query string
     String url = request.getParameter("url");
     String format = request.getParameter("format");
+    
+    // Next let's test to see what arrived.
+    if(!format.isEmpty() && format == "xml") {
+      // send XML
+      response.setContentType("text/xml");
+    } else {
+      // send JSON
+      response.setContentType("application/json");
+    }
+    
+    // Definitely want to whitelist this; might be best to set it through external config
+    if(!url.isEmpty() && url.contains()) {
+      
+    } else {
+      // No need to open this up as an external service, right?  
+    }
   }
 }
