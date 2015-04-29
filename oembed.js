49b7ce94ae970d09998ccc24dc42a2bc6be0ef40
Aaron Helton <aaronhelton@Aarons-MacBook-Pro.local>
2015-04-29 15:11:22 -0400

    examples provisionally complete

--- oembed.js
+++ oembed.js
@@ -1,6 +1,14 @@
 if (!window.jQuery) {
 	var jq = document.createElement('script'); jq.type = 'text/javascript';
 	jq.src = 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js';
-	document.getElementByTagName('head')[0].appendChild(jq);
+	document.getElementsByTagName('head')[0].appendChild(jq);
 }
 
+function showWidget(url) {
+	// url here should be coming from the page that holds the embed button
+	$.getJSON("http://localhost:9292/dsoembed?url=" + url, function( data ) {
+		html_src = data["html"];
+		$('#widget').append('<textarea>' + html_src + '</textarea>');
+		$('#wdiget').show();
+	});
+}
