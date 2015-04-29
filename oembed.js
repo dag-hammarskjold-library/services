24a981ee2a3cfa43ea124370ef808355c97f2127
Aaron Helton <aaronhelton@Aarons-MacBook-Pro.local>
2015-04-29 14:30:42 -0400

    build out client js and example html

---
+++ oembed.js
@@ -0,0 +1,6 @@
+if (!window.jQuery) {
+	var jq = document.createElement('script'); jq.type = 'text/javascript';
+	jq.src = 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js';
+	document.getElementByTagName('head')[0].appendChild(jq);
+}
+
